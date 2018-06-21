091afc666af5f0aa777665900e60d1de553c16e5
kevinccbsg <kevinccbsg@gmail.com>
2018-06-20 22:32:47 -0400

    Refactor  - Refactor App component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,24 +1,13 @@
 import React, { Component } from 'react';
 import PhoneListContainer from './PhoneListContainer'
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      phones: [],
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <header>
-          <h2>Phone searcher</h2>
-        </header>
-        <PhoneListContainer />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <header>
+      <h2>Phone searcher</h2>
+    </header>
+    <PhoneListContainer />
+  </div>
+);
 
 export default App;
